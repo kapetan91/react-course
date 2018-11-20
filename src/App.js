d5d48c80197406334a34844494ae1f4babcd6463
goranJankovic <goran.jankovic@4zida.rs>
2018-11-20 12:43:25 +0100

    outputting dynamic content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
+          <h1>Another reading</h1>
+          <p>This is really working</p>
+          <Person />
+	      <Person />
+	      <Person />
       </div>
     );
+    //   return React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'goran'));
   }
 }
 
